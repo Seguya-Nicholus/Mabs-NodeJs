c755cb7e476bd16374b2bdd9485cbc5b1ae1066f
Seguya Nicholus <seguyanicholus2015@gmail.com>
2019-11-25 14:15:43 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+// Requiring the packages
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+// Create our express app
+const app = express();
+
+// Set the engine that is to render files
+app.set("view engine", "pug");
+
+// Set the path where to find the pages to render
+app.set("views", path.join(__dirname, "views"));
+
+// Let the app use the body parser package
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// mongoose db connection
+mongoose.connect("mongodb://localhost:27017/node-project", () => { console.log('Database Connection Successful') });
+
+
+
+
+//  Listening for requests: the server
+app.listen(5000, () => {
+    console.log("App Listening on Port 5000");
+
+})
