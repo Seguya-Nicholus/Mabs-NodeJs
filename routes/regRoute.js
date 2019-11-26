b6a6371a73211fd4f21e4f5c79fc25f2b533a6e8
Tumwesigye <Tumwesigye>
2019-11-26 09:29:10 +0300

    Created and rendered the login page

---
+++ routes/regRoute.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+//Render the login form on the path /form
+router.get('/', (req, res) => {
+    res.render('login');
+})
+
+//eport routes.
+module.exports = router;
